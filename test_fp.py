77a4aac1d094abebe5903d5086618ea221803f31
muhirza <muhammadirza31@gmail.com>
2022-08-01 16:25:23 +0700

    Update test_fp.py

--- test_fp.py
+++ test_fp.py
@@ -5,17 +5,79 @@ import time
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
 
-class TestLogin(unittest.TestCase): 
+class TestOrange(unittest.TestCase): 
 
     def setUp(self): 
-        self.browser = webdriver.Chrome(ChromeDriverManager().install()) #deklarasi browser
+        self.browser = webdriver.Chrome(ChromeDriverManager().install()) 
+        #deklarasi browser
         browser= self.browser
         browser.maximize_window()
         
     #nama depan def HARUS test
     # menjalankan berdasarkan ABJAD. makanya ada test_a, test_b, dll    
-    def test_a_success_login(self): 
+    def test_a_login_blank(self): 
+        # steps
+        # WebElement chooseFile = driver.findElement(By.id("custom-input"));
+        browser = self.browser #buka web browser
+        browser.get("https://opensource-demo.orangehrmlive.com/ ") # buka situs
+        time.sleep(3)
+        browser.find_element(By.ID,"txtUsername").send_keys("") # isi email
+        time.sleep(1)
+        browser.find_element(By.ID,"txtPassword").send_keys("") # isi password
+        time.sleep(1)
+        browser.find_element(By.ID,"btnLogin").click() # klik tombol sign in
+        time.sleep(3)
+
+        # validasi
+        response_data = browser.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/div[2]/div[2]/form/div[5]/span").text
+        # response_message = browser.find_element(By.ID,"swal2-content").text
+        
+        # self.assertEqual(True,response_data)
+        self.assertIn('empty', response_data)
+
+    def test_b_login_wrong_username(self): 
+        # steps
+        # WebElement chooseFile = driver.findElement(By.id("custom-input"));
+        browser = self.browser #buka web browser
+        browser.get("https://opensource-demo.orangehrmlive.com/ ") # buka situs
+        time.sleep(3)
+        browser.find_element(By.ID,"txtUsername").send_keys("asiap") # isi email
+        time.sleep(1)
+        browser.find_element(By.ID,"txtPassword").send_keys("admin123") # isi password
+        time.sleep(1)
+        browser.find_element(By.ID,"btnLogin").click() # klik tombol sign in
+        time.sleep(3)
+
+        # validasi
+        response_data = browser.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/div[2]/div[2]/form/div[5]/span").text
+        # response_message = browser.find_element(By.ID,"swal2-content").text
+        
+        # self.assertEqual(True,response_data)
+        self.assertIn('Invalid ', response_data)
+    
+    def test_c_login_wrong_username(self): 
+        # steps
+        # WebElement chooseFile = driver.findElement(By.id("custom-input"));
+        browser = self.browser #buka web browser
+        browser.get("https://opensource-demo.orangehrmlive.com/ ") # buka situs
+        time.sleep(3)
+        browser.find_element(By.ID,"txtUsername").send_keys("Admin") # isi email
+        time.sleep(1)
+        browser.find_element(By.ID,"txtPassword").send_keys("admin1234") # isi password
+        time.sleep(1)
+        browser.find_element(By.ID,"btnLogin").click() # klik tombol sign in
+        time.sleep(3)
+
+        # validasi
+        response_data = browser.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/div[2]/div[2]/form/div[5]/span").text
+        # response_message = browser.find_element(By.ID,"swal2-content").text
+        
+        # self.assertEqual(True,response_data)
+        self.assertIn('Invalid ', response_data)
+
+    def test_d_login_success(self): 
         # steps
         # WebElement chooseFile = driver.findElement(By.id("custom-input"));
         browser = self.browser #buka web browser
@@ -26,14 +88,42 @@ class TestLogin(unittest.TestCase):
         browser.find_element(By.ID,"txtPassword").send_keys("admin123") # isi password
         time.sleep(1)
         browser.find_element(By.ID,"btnLogin").click() # klik tombol sign in
-        time.sleep(1)
+        time.sleep(3)
 
         # validasi
-        response_data = browser.find_element(By.XPATH,"/html/body/div[1]/div[1]/a[2]").is_displayed
+        response_data = browser.find_element(By.XPATH,"/html/body/div[1]/div[3]/div/div[1]/h1").text
         # response_message = browser.find_element(By.ID,"swal2-content").text
-        print(response_data)
-        self.assertEqual(True,response_data)
-        # self.assertIn('Marketplace', response_data)
+        
+        # self.assertEqual(True,response_data)
+        self.assertIn('Dashboard', response_data)
+    
+    def test_e_admin(self): 
+        # steps
+        # WebElement chooseFile = driver.findElement(By.id("custom-input"));
+        browser = self.browser #buka web browser
+        browser.get("https://opensource-demo.orangehrmlive.com/ ") # buka situs
+        time.sleep(3)
+        browser.find_element(By.ID,"txtUsername").send_keys("Admin") # isi email
+        time.sleep(1)
+        browser.find_element(By.ID,"txtPassword").send_keys("admin123") # isi password
+        time.sleep(1)
+        browser.find_element(By.ID,"btnLogin").click() # klik tombol sign in
+        time.sleep(3)
+        admin_tombol = browser.find_element(By.ID,"menu_admin_viewAdminModule")
+        tombol_user_management = browser.find_element(By.ID,"menu_admin_UserManagement")
+        tombol_view_user = browser.find_element(By.ID,"menu_admin_viewSystemUsers")
+        ActionChains(browser).move_to_element(admin_tombol).move_to_element(tombol_user_management).click(tombol_view_user).perform()
+        time.sleep(3)
+        browser.find_element(By.ID,"searchSystemUser_userName").send_keys("Jacqueline W") 
+        time.sleep(1)
+        browser.find_element(By.ID,"searchBtn").click()
+        time.sleep(5)
+    
+        # validasi
+        response_data = browser.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[2]/div/div/form/div[4]/table/tbody/tr/td[3]").text
+        self.assertIn("Admin",response_data)
+
+        
 
     # def test_b_failed_login_with_empty_password(self): 
     #     # steps
